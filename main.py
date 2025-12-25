85499b4e895ced5b339ffd66cc1b1ed6ba28a8be
wd-coder-111 <adugaming2829@gmail.com>
2025-12-25 14:10:44 +0530

    Update MIN_CHANNEL_ID to a new value

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1003459220099
+pyrogram.utils.MIN_CHANNEL_ID = 1003210179465
 
 if __name__ == "__main__":
     Bot().run()
